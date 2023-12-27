de620f3850aee07c728496e82e7ae22eeac7d7fb
rohit <rohit.gosain@infobeans.com>
2023-12-27 17:02:39 +0530

    first commit

---
+++ src/app/pages/audit/audit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit',
+  standalone: true,
+  imports: [],
+  templateUrl: './audit.component.html',
+  styleUrl: './audit.component.scss'
+})
+export class AuditComponent {
+
+}
