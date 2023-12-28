932c95541b07b84e1ceabd976428c97e6a7346dd
rohit680086 <rohit680086@gmail.com>
2023-12-28 14:44:38 +0530

    merge changes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = []= [
+    {
+      path: 'features',
+      loadComponent: () => import('./pages/features/features.component').then(c => c.FeaturesComponent)
+    },
+    {
+        path: 'companies',
+        loadComponent: () => import('./pages/companies/companies.component').then(c => c.CompaniesComponent)
+      },
+      {
+        path: 'requests',
+        loadComponent: () => import('./pages/requests/requests.component').then(c => c.RequestsComponent)
+      },
+      {
+        path: 'audit',
+        loadComponent: () => import('./pages/audit/audit.component').then(c => c.AuditComponent)
+      },
+    {
+      path: '',
+    //   pathMatch:'full', 
+      loadComponent: () => import('./pages/features/features.component').then(c => c.FeaturesComponent)
+    },
+    // {
+    //   path: '**',
+    //   loadComponent: () => import('./page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)
+    // }
+];
--- src/app/pages/features/features.component.ts
+++ src/app/pages/features/features.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './features.component.html',
   styleUrl: './features.component.scss'
 })
 export class FeaturesComponent {
-
+  title = 'pure_feature_flag';
 }
